d937d24c647c0c303cec4c63fa4ab193b910d53b
Ariel Alvarez <arialvarez2019@gmail.com>
2023-04-10 18:00:36 -0300

    segunda parte practica 1-6

--- algoritmo.js
+++ algoritmo.js
@@ -1,5 +1,17 @@
-let colorFavorito = prompt("Cual es tu color favorito?");
+let numeroEntero = prompt("ingresar el tipo de dato integer/entero:");
+let booleano = prompt("ingresar el tipo de dato booleano:");
+let numeroDecimal = prompt("ingresar un numero decimal:");
+let caracter = prompt("ingresar un caracter:");
+let palabra = prompt("ingresar una palabra");
 
-console.log( colorFavorito );
+document.write("<h1> Entero:"+ numeroEntero + "</h1>");
+document.write("<h2> Booleano:"+ booleano + "</h2>");
+document.write("<h3> Decimal:"+ numeroDecimal + "</h3>");
+document.write("<h4> Caracter:"+ caracter + "</h4>");
+document.write("<h5> Palabra(Conjunto de caracteres):"+ palabra + "</h5>");
+
+/* let colorFavorito = prompt("Ingresar el tipo de dato integer/entero:"); */
+
+/* console.log( colorFavorito );
 document.querySelector("body").innerHTML = "<h1> "+ colorFavorito + " </h1>";
-document.write("<h2> "+ colorFavorito + " </h2>");
+document.write("<h2> "+ colorFavorito + " </h2>"); */
