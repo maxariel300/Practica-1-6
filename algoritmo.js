2c1096d61107003d82f5e964edcb5d8b5cbf2999
Ariel Alvarez <arialvarez2019@gmail.com>
2023-04-10 16:06:16 -0300

    primer parte 1-6 terminado

---
+++ algoritmo.js
@@ -0,0 +1,5 @@
+let colorFavorito = prompt("Cual es tu color favorito?");
+
+console.log( colorFavorito );
+document.querySelector("body").innerHTML = "<h1> "+ colorFavorito + " </h1>";
+document.write("<h2> "+ colorFavorito + " </h2>");
